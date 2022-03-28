964d4a19aa42ef22c45e77d62d8e985a34e83cb6
Ian Marsman <ian.marsman@gmail.com>
2022-03-28 18:46:59 -0400

    Output loops plus result

--- test/fibonacci.sh
+++ test/fibonacci.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-term="$((1 + $RANDOM % 20))"
+# e.g. time concur './fibonacci.sh' -a '{1..1000}' \
+#        -A '{printf "%-10s", "cycles:"$1; printf "%-10s ",  " result:"$2}'
+
+term="$((1 + $RANDOM % 30))"
 
 i=0
 j=1
@@ -8,7 +11,6 @@ for (( c=0; c<term; c++ )) ; do
     (( n=i+j ))
     (( i=j ))
     (( j=n ))
-#    echo $i
 done
 
-echo "${i}"
+echo "${term} ${i}"
